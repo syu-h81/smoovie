269e5b709fe42d0001db53da40b12946c1417f56
Danjo Syu <>
2025-03-04 03:58:40 +0900

    first commit

---
+++ src/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+
+  <!-- OGP -->
+  <meta property="og:url" content=""> <!-- //ページのURLを設定 -->
+  <meta property="og:type" content=""> <!-- //ページの種類を設定 -->
+  <meta property="og:title" content=""> <!-- //ページのタイトルを設定 -->
+  <meta property="og:description" content=""> <!-- //ページのディスカッションを設定 -->
+  <meta property="og:site_name" content=""> <!-- //サイトの名前を設定 -->
+  <meta property="og:image" content=""> <!-- //ogp画像の設定 -->
+  <meta property="og:locale" content="ja_JP"> <!-- //ページの国コードの設定 -->
+
+  <!-- Import Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet">
+
+  <!-- Import CSS -->
+  <link rel="stylesheet" href="assets/css/style.css">
+
+  <!-- Import JS/jQuery Library -->
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+          integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+          crossorigin="anonymous">
+  </script>
+</head>
+<body>
+  <div class="wrapper">
+    <!--======== l-header ========-->
+    <header class="l-header"> 
+      <a href=""></a>
+    </header>
+    <!--======== l-footer ========-->
+    <footer class="l-footer">
+    </footer>
+  </div>
+
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const webpack = require('webpack');
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
+
+module.exports = {
+  // コンパイルモード
+  mode: 'production',
+  // エントリーポイントの設定
+  entry: {
+    // コンパイル対象のファイルを指定
+    'main': path.resolve(__dirname, './src/assets/js/main.js'),
+    'style.css': path.join(__dirname, './src/assets/css/styles.css'),
+  },
+  // 出力設定
+  output: {
+      path: path.resolve(__dirname, './dist/'), // 出力先フォルダを絶対パスで指定
+      filename: 'assets/js/[name].js'// [name]にはentry:で指定したキーが入る
+  },
+  module: {
+    rules: [
+      // sassのコンパイル設定
+      {
+        test: /\.(sa|sc|c)ss$/, // 対象にするファイルを指定
+        use: [
+          MiniCssExtractPlugin.loader, // JSとCSSを別々に出力する
+          {
+            loader: 'css-loader',
+            options: {
+              import: false,
+              url: false, //URL の解決を無効に
+            }
+          },
+          'postcss-loader', // オプションはpostcss.config.jsで指定
+          'sass-loader',
+          // 下から順にコンパイル処理が実行されるので、記入順序に注意
+        ]
+      },
+      {
+        // Babel 用のローダー
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new TerserPlugin(), //JSファイルの圧縮
+    new FixStyleOnlyEntriesPlugin(), // CSS別出力時の不要JSファイルを削除
+    new CssMinimizerPlugin(), // CSSファイルの圧縮
+    new MiniCssExtractPlugin({ // CSSの出力先
+      filename: 'assets/css/[name]'// 出力ファイル名を相対パスで指定（[name]にはentry:で指定したキーが入る）
+    }),
+    //htmlファイルの出力の設定はここ
+    new HtmlWebpackPlugin({ //htmlファイルを出力する場合に新しく追加する
+      template: './src/index.php',
+      filename: './index.php',
+    }),
+    //PHPファイル、画像ファイルの出力
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/assets/images', to: 'assets/images' },
+      ]
+    }),
+  ],
+  // node_modules を監視（watch）対象から除外
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+  }
+};
